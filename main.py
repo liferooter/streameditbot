8446e7f706f7e0bd106a618d05fd1d8cdd8807d7
Liferooter <glebsmirnov0708@gmail.com>
2020-12-04 21:01:46 +0300

    Better documentation, more comments

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import logging
 import os
 import asyncio
-from asyncio.subprocess import Process
-from shlex import split
 from html import escape
 from aiogram import Bot, Dispatcher, executor, types
 
 BOT_TOKEN: str = os.getenv("BOT_TOKEN")
 MSG_LENGTH_LIMIT = 2 ** 12
-SANDBOX_USER='bot'
+SANDBOX_USER = 'bot'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -20,33 +18,40 @@ dp = Dispatcher(bot)
 COMMANDS = ['sed', 'grep', 'cut', 'tr', 'tail', 'head', 'uniq', 'sort', 'awk']
 
 
-@dp.message_handler(regexp=f'^({"|".join(COMMANDS)})')
-async def cmd_handler(message: types.Message):
-    if message.reply_to_message:
-        input_text = message.reply_to_message.text.encode('utf-8')
-    else:
-        input_text = b''
-
-    cmdline = message.text
-    proc = await asyncio.create_subprocess_exec("su", SANDBOX_USER, "-c", f"/usr/src/app/sandbox.sh {cmdline}",
+async def run_in_container(cmd: str, stdin: str) -> (str, str, int):
+    """
+    Run program in container.
+    Returns stdout, stderr and exit_code.
+    """
+    proc = await asyncio.create_subprocess_exec("su", SANDBOX_USER, "-c", f"/usr/src/app/sandbox.sh {cmd}",
                                                 stdout=asyncio.subprocess.PIPE,
                                                 stderr=asyncio.subprocess.PIPE,
                                                 stdin=asyncio.subprocess.PIPE)
-    stdout, stderr = await proc.communicate(input_text)
+    stdout, stderr = await proc.communicate(stdin.encode("utf-8"))
+    return stdout.decode('utf-8', errors='ignore'), stderr.decode('utf-8', errors='ignore'), 0  # TODO: exit code
+
+
+@dp.message_handler(regexp=f'^({"|".join(COMMANDS)})')
+async def cmd_handler(message: types.Message):
+
+    stdout, stderr, _ = await run_in_container(message.text,
+                                               message.reply_to_message.text if message.reply_to_message else "")
 
     # Telegram message length limit
     if len(stdout) >= MSG_LENGTH_LIMIT:
-        stdout = b"Output is too long"
+        stdout = "Output is too long"
     if len(stderr) >= MSG_LENGTH_LIMIT:
-        stderr = b"Error is too long"
+        stderr = "Error is too long"
 
     if stderr:
-        await message.reply(f'<pre>{escape(stderr.decode("utf-8", errors="ignore"))}</pre>')
+        await message.reply(
+            f'<pre>{escape(stderr)}</pre>'
+        )  # TODO: return stderr if exit code != 0
     elif stdout:
         if message.reply_to_message:
-            await message.reply_to_message.reply(escape(stdout.decode("utf-8", errors="ignore")))
+            await message.reply_to_message.reply(escape(stdout))
         else:
-            await message.reply(escape(stdout.decode('utf-8', errors='ignore')))
+            await message.reply(escape(stdout))
     else:
         await message.reply("<pre>Output is empty...</pre>")
 
