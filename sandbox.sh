1fcad2b3af8f039f97b893571a761ac3bd044111
Liferooter <glebsmirnov0708@gmail.com>
2020-12-04 14:32:40 +0300

    New sanbox expression escaping

--- sandbox.sh
+++ sandbox.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 ulimit -Sv 50000 # ~= 50MB memory limit
-env -i PATH=/bin:/usr/bin timeout 2 bash -c "$*" # Erase environment, set time limit and run
+
+# Erase environment, set time limit and run# Erase environment, set time limit and run
+eval "$(printf 'env -i PATH=/bin:/usr/bin timeout 1 %q' "$*" | sed  's/\\\ / /g')"
